eea7597ab316b29fb79e8750ee4d78b9779cb6db
Cristian Larios <96441511+Fas5ter@users.noreply.github.com>
2022-08-28 13:39:52 -0500

    sumar.py
    Funcion que pide dos datos de tipo int o float y retorna la suma de estos dos datos dados

---
+++ Dia26Ago2022/ops/sumar.py
@@ -0,0 +1,2 @@
+def sumar(a:int|float, b:int|float) -> int|float:   # Se crea una funcion que pide un dato de tipo int o float y retorna un valor int o float
+	return a+b    # Retorna el valor de la suma de los dos datos dados
