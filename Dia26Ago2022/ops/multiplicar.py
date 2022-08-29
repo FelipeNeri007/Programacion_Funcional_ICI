1231098cbe32cf38fe8fe112f97538e65b6b9d67
Cristian Larios <96441511+Fas5ter@users.noreply.github.com>
2022-08-28 19:45:36 -0500

    Update multiplicar.py

--- Dia26Ago2022/ops/multiplicar.py
+++ Dia26Ago2022/ops/multiplicar.py
@@ -1,2 +1,3 @@
 def multiplicar(a:int|float, b:int|float)->int|float:   # Se crea una funcion que pide dos datos de tipo int o float y retorna un valor int o float
     return a*b    # Retorna el valor de la multiplicacion de los dos datos dados
+# Cristian Larios ©
