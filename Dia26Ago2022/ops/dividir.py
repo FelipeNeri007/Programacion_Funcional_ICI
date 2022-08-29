de57fa96f7f7c4529d4e34afb0025df018e018e8
Cristian Larios <96441511+Fas5ter@users.noreply.github.com>
2022-08-28 19:45:24 -0500

    Update dividir.py

--- Dia26Ago2022/ops/dividir.py
+++ Dia26Ago2022/ops/dividir.py
@@ -1,2 +1,3 @@
 def dividir(a:int|float, b:int|float)->int|float: # Se crea una funcion que pide dos datos de tipo int o float y retorna un valor int o float
     return a/b  # Retorna la division del primer dato entre el segundo dato dado
+# Cristian Larios ©
