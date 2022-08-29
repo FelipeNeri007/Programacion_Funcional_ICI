ea49a387ff01d8090bbc6e613cc53bd1d0e3c239
Cristian Larios <96441511+Fas5ter@users.noreply.github.com>
2022-08-28 19:46:01 -0500

    Update restar.py

--- Dia26Ago2022/ops/restar.py
+++ Dia26Ago2022/ops/restar.py
@@ -1,2 +1,3 @@
 def restar(a:int|float, b:int|float)->int|float:    # Se crea una funcion que pide un dato de tipo int o float y retorna un valor int o float
 	return a-b    # Retorna el valor de la resta del primer dato menos el segundo dato dado
+# Cristian Larios ©
