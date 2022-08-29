b860727214becedc67213873c056235bcc19b805
Cristian Larios <96441511+Fas5ter@users.noreply.github.com>
2022-08-28 19:45:12 -0500

    Update cuadrado.py

--- Dia26Ago2022/ops/cuadrado.py
+++ Dia26Ago2022/ops/cuadrado.py
@@ -1,2 +1,3 @@
 def cuadrado(a:int|float)->int|float:   # Se crea una funcion que pide un dato de tipo int o float y retorna un valor int o float
     return a*a   # Retorna el cuadrado de el numero dado
+# Cristian Larios ©
