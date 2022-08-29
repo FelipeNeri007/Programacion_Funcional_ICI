087580f6d2ac7b54ccbf300a703a036fac0b712b
Cristian Larios <96441511+Fas5ter@users.noreply.github.com>
2022-08-28 19:45:49 -0500

    Update operaciones.py

--- Dia26Ago2022/ops/operaciones.py
+++ Dia26Ago2022/ops/operaciones.py
@@ -12,3 +12,4 @@ def multiplicar(a:int|float, b:int|float)->int|float:   # Se crea una funcion qu
 
 def dividir(a:int|float, b:int|float)->int|float:   # Se crea una funcion que pide dos datos de tipo int o float y retorna un valor int o float
     return a/b    # Retorna la division del primer dato entre el segundo dato dado
+# Cristian Larios ©
