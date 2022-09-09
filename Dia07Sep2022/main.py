27d2e8922f8de71300db87d092407ae3fb52801a
Cristian Larios <96441511+Fas5ter@users.noreply.github.com>
2022-09-08 20:41:41 -0500

    Update main.py

--- Dia07Sep2022/main.py
+++ Dia07Sep2022/main.py
@@ -1,21 +1,21 @@
-lista1 = [1, 2, 3, 4]
+lista1 = [1, 2, 3, 4]   # Crear una lista
 print(lista1)
 
-lista2 = [1, 3.14, "a", True, [1,2,3], (1, 2, 3), {8, "a", 5.4}]
+lista2 = [1, 3.14, "a", True, [1,2,3], (1, 2, 3), {8, "a", 5.4}]        # Una lista puede contener cualquier tipo de dato
 print(lista2)
 
 print(len(lista1))
 
 print(len(lista2))
-print(lista2[6])
+print(lista2[6])        # Acceder a un elemento de la lista
 
 lista_cal = []
 print(lista_cal)
-lista_cal.append(5)
-print(lista_cal)
+lista_cal.append(5)     # Agregar un elemento al final de la lista
+print(lista_cal)   
 lista_cal.append(8)
 print(lista_cal)
-lista_cal.insert(0,6)
+lista_cal.insert(0,6)   # Modificar el elemento del indice 0
 lista_cal.append(5)
 print(lista_cal)
 
@@ -87,3 +87,4 @@ del lista1[0]  # Eliminar la lista1 indice 0
 print(lista1)
 del(lista1[2:5])
 print(lista1)
+#Cristian Larios ©
